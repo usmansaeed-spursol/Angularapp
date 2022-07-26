aed7f9e592ba45361b08eebcd078d211a5ea6514
usmansaeed-spursol <110039214+usmansaeed-spursol@users.noreply.github.com>
2022-07-26 19:02:39 +0500

    Folder 1-10 26 july updated

--- Newapp/RecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Newapp/RecipeApp/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -9,17 +9,12 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
 
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes !: Recipe[];
 
   constructor(private recipeService : RecipeService) { }
 
   ngOnInit(): void {
     this.recipes = this.recipeService.getRecipes();
-  }
-
-  onRecipeSelected(recipe:Recipe){
-    this.recipeWasSelected.emit(recipe);
   }
 
 }
--- Newapp/RecipeApp/src/app/recipes/recipe.service.ts
+++ Newapp/RecipeApp/src/app/recipes/recipe.service.ts
@@ -1,7 +1,10 @@
+import { EventEmitter } from "@angular/core";
 import { Recipe } from "./recipe.model";
 
 
 export  class RecipeService{
+  recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('Pasta','Test description','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.826.620.suffix/1615916524567.jpeg'),
         new Recipe('2nd Pasta','Test description','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.826.620.suffix/1615916524567.jpeg')
