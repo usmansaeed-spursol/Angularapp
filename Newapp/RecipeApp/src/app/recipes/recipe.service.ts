bf1535893887c381f30029fe308bb7e1918e3f0a
usmansaeed-spursol <110039214+usmansaeed-spursol@users.noreply.github.com>
2022-07-27 11:17:07 +0500

    Folder 10 done 27 july Tutorial App

--- Newapp/RecipeApp/src/app/recipes/recipe.service.ts
+++ Newapp/RecipeApp/src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 import { Recipe } from "./recipe.model";
 
 
@@ -6,8 +7,20 @@ export  class RecipeService{
   recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('Pasta','Test description','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.826.620.suffix/1615916524567.jpeg'),
-        new Recipe('2nd Pasta','Test description','https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.826.620.suffix/1615916524567.jpeg')
+        new Recipe('Pasta',
+        'Test description',
+        'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.826.620.suffix/1615916524567.jpeg',
+        [
+          new Ingredient('Meat',1),
+          new Ingredient('French Fries',20)
+        ]),
+        new Recipe('2nd Pasta'
+        ,'Test description'
+        ,'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2021/02/05/Baked-Feta-Pasta-4_s4x3.jpg.rend.hgtvcom.826.620.suffix/1615916524567.jpeg',
+        [
+          new Ingredient('Meat',1),
+          new Ingredient('Buns',2)
+        ])
       ];
 
       getRecipes(){
